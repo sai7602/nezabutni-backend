32e1cde8ee78e7488349172d5ed5fac1e2b02a88
Oleksandr Salivon <sai7602@ukr.net>
2023-06-15 15:11:19 +0300

    add API

---
+++ pages/api/dementia-types.ts
@@ -0,0 +1,98 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '@/lib/prisma';
+
+/**
+ * @swagger
+ *
+ * /api/dementia-types:
+ *   get:
+ *     tags: [DementiaMenu]
+ *     summary: Отримання даних для сторінки Що таке деменція.
+ *
+ *     responses:
+ *       200:
+ *         description: Successful response with the list of dementia menus.
+ *         content:
+ *           application/json:
+ *              schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                 menuTitle:
+ *                   type: string
+ *                 shortContent:
+ *                   type: string
+ *                 linkUrl:
+ *                   type: string
+ *                 contentLight:
+ *                   type: string
+ *                 contentDark:
+ *                   type: string
+ *                 menuSubTitle:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       SubMenuTitle:
+ *                         type: string
+ *                       SubMenuId:
+ *                         type: integer
+ *                       SubMenuCards:
+ *                         type: array
+ *                         items:
+ *                           type: object
+ *                           properties:
+ *                             cardText:
+ *                               type: array
+ *                               items:
+ *                                 type: object
+ *                                 properties:
+ *                                   cardTextTitle:
+ *                                     type: string
+ *                                   cardTextId:
+ *                                     type: integer
+ *                             cardTitle:
+ *                               type: string
+ *                             cardId:
+ *                               type: integer
+ *     security: []
+ */
+
+export default async function handler(
+	request: NextApiRequest,
+	response: NextApiResponse
+) {
+	try {
+		const { id } = request.query;
+
+		const result = await prisma.dementiaMenu.findMany({
+			where: { menuTitle: 'Типи деменції' },
+			include: {
+				menuSubTitle: {
+					select: {
+						SubMenuTitle: true,
+						SubMenuId: true,
+						SubMenuCards: {
+							select: {
+								cardText: {
+									select: {
+										cardTextTitle: true,
+										cardTextId: true,
+									},
+								},
+								cardTitle: true,
+								cardId: true,
+							},
+						},
+					},
+				},
+			},
+		});
+
+		return response.status(200).json(result);
+	} catch (error) {
+		console.error(error);
+		return response.status(500).json({ error: 'Something went wrong' });
+	}
+}
